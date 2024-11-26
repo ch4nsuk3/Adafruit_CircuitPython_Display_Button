89e804b08065c9000c6b89318bf07ef7dd843df8
ch4nsuk3 <134003603+ch4nsuk3@users.noreply.github.com>
2024-11-26 12:32:32 -0600

    button_base annotations in place
    Added annotations for the various functions. Corrected some documentation errors as well, regarding what the selected_label actually does. Additionally added terminalio.FONT as the default font.

--- adafruit_button/button_base.py
+++ adafruit_button/button_base.py
@@ -22,9 +22,15 @@ Implementation Notes
 """
 from adafruit_display_text.bitmap_label import Label
 from displayio import Group
+import terminalio
+try:
+    from typing import Optional, Union, List, Tuple, Any
+    from fontio import FontProtocol
+except ImportError:
+    pass
 
 
-def _check_color(color):
+def _check_color(color: Optional[Union[int, tuple[int, int, int]]]) -> Optional[int]:
     # if a tuple is supplied, convert it to a RGB number
     if isinstance(color, tuple):
         r, g, b = color
@@ -36,31 +42,31 @@ class ButtonBase(Group):
     # pylint: disable=too-many-instance-attributes
     """Superclass for creating UI buttons for ``displayio``.
 
-    :param x: The x position of the button.
-    :param y: The y position of the button.
-    :param width: The width of the button in tiles.
-    :param height: The height of the button in tiles.
-    :param name: A name, or miscellaneous string that is stored on the button.
-    :param label: The text that appears inside the button. Defaults to not displaying the label.
-    :param label_font: The button label font.
-    :param label_color: The color of the button label text. Defaults to 0x0.
-    :param selected_label: Text that appears when selected
+    :param int x: The x position of the button.
+    :param int y: The y position of the button.
+    :param int width: The width of the button in tiles.
+    :param int height: The height of the button in tiles.
+    :param str name: A name, or miscellaneous string that is stored on the button.
+    :param str label: The text that appears inside the button. Defaults to not displaying the label.
+    :param FontProtocol label_font: The button label font.
+    :param int label_color: The color of the button label text. Defaults to 0x0.
+    :param selected_label: The color of button label text when the button is selected.
     """
 
     def __init__(
         self,
         *,
-        x,
-        y,
-        width,
-        height,
-        name=None,
-        label=None,
-        label_font=None,
-        label_color=0x0,
-        selected_label=None,
-        label_scale=None
-    ):
+        x: int,
+        y: int,
+        width: int,
+        height: int,
+        name: Optional[str] = None,
+        label: Optional[str] = None,
+        label_font: Optional[FontProtocol] = None,
+        label_color: Optional[int] = 0x0,
+        selected_label: Optional[Union[int, tuple[int, int, int]]] = None,
+        label_scale: Optional[int] = 1,
+    ) -> None:
         super().__init__(x=x, y=y)
         self.x = x
         self.y = y
@@ -73,15 +79,15 @@ class ButtonBase(Group):
         self._label_color = label_color
         self._label_font = label_font
         self._selected_label = _check_color(selected_label)
-        self._label_scale = label_scale or 1
+        self._label_scale = label_scale
 
     @property
-    def label(self):
+    def label(self) -> Optional[Tuple[str, str]]:
         """The text label of the button"""
         return getattr(self._label, "text", None)
 
     @label.setter
-    def label(self, newtext):
+    def label(self, newtext: str) -> None:
         if self._label and self and (self[-1] == self._label):
             self.pop()
 
@@ -90,7 +96,7 @@ class ButtonBase(Group):
             return  # nothing to do!
 
         if not self._label_font:
-            raise RuntimeError("Please provide label font")
+            self._label_font = terminalio.FONT
         self._label = Label(self._label_font, text=newtext, scale=self._label_scale)
         dims = list(self._label.bounding_box)
         dims[2] *= self._label.scale
@@ -115,17 +121,17 @@ class ButtonBase(Group):
         if (self.selected_label is None) and (self._label_color is not None):
             self.selected_label = (~self._label_color) & 0xFFFFFF
 
-    def _subclass_selected_behavior(self, value):
-        # Subclasses should overide this!
+    def _subclass_selected_behavior(self, value: Optional[Any]) -> None:
+        # Subclasses should override this!
         pass
 
     @property
-    def selected(self):
+    def selected(self) -> bool:
         """Selected inverts the colors."""
         return self._selected
 
     @selected.setter
-    def selected(self, value):
+    def selected(self, value: bool) -> None:
         if value == self._selected:
             return  # bail now, nothing more to do
         self._selected = value
@@ -140,20 +146,20 @@ class ButtonBase(Group):
         self._subclass_selected_behavior(value)
 
     @property
-    def selected_label(self):
+    def selected_label(self) -> int:
         """The font color of the button when selected"""
         return self._selected_label
 
     @selected_label.setter
-    def selected_label(self, new_color):
+    def selected_label(self, new_color: int) -> None:
         self._selected_label = _check_color(new_color)
 
     @property
-    def label_color(self):
+    def label_color(self) -> int:
         """The font color of the button"""
         return self._label_color
 
     @label_color.setter
-    def label_color(self, new_color):
+    def label_color(self, new_color: int) -> None:
         self._label_color = _check_color(new_color)
         self._label.color = self._label_color
